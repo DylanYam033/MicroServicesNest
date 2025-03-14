182794d2e201279b09d68b8ba4df4ca028c22a7e
Dylan <dylan.yampuezan@wactechnology.com>
2025-03-14 10:50:58 -0500

    feat(api): seperacion metodos creacion pdf y cambio el url

--- src/generate-pdf/generate-pdf.service.ts
+++ src/generate-pdf/generate-pdf.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@nestjs/common';
 import * as puppeteer from 'puppeteer';
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 import { format as formatDate } from 'date-fns';
-import { PDFOptions, PaperFormat } from 'puppeteer'; 
+import { PDFOptions, PaperFormat } from 'puppeteer';
+import { Buffer } from 'buffer';
 
 @Injectable()
 export class GeneratePdfService {
@@ -20,8 +21,7 @@ export class GeneratePdfService {
         });
     }
 
-    async generateAndUploadPDF(htmlContent: string, user: any): Promise<{ pdfUrl: string; s3Path: string }> {
-        // Lanzar Puppeteer sin guardar en disco
+    async generatePDF(htmlContent: string): Promise<Buffer> {
         const browser = await puppeteer.launch({ headless: true });
         const page = await browser.newPage();
         await page.setContent(htmlContent, { waitUntil: 'networkidle0' });
@@ -34,16 +34,16 @@ export class GeneratePdfService {
             margin: { top: '0', right: '0', bottom: '0', left: '0' },
         };
 
-        // Generar PDF en memoria
         const pdfBuffer = await page.pdf(pdfOptions);
-
         await browser.close();
 
-        // Generar nombre del archivo
+        return Buffer.from(pdfBuffer); // Conversión a Buffer para evitar el error
+    }
+
+    async uploadPDF(pdfBuffer: Buffer, user: any): Promise<{ pdfUrl: string; s3Path: string }> {
         const fileName = `comprobante_pago_estudio_${user.nombreUsuario}_${user.cedula}_${formatDate(new Date(), 'yyyyMMddHHmmss')}.pdf`;
         const s3Path = `MyManager/Nomina/DesprendiblePago/${fileName}`;
 
-        // Subir el PDF a S3 directamente desde memoria
         const uploadParams = {
             Bucket: this.bucketName,
             Key: s3Path,
@@ -53,9 +53,12 @@ export class GeneratePdfService {
 
         await this.s3Client.send(new PutObjectCommand(uploadParams));
 
-        // Construir la URL del archivo en S3
         const pdfUrl = `https://${this.bucketName}.s3.${this.region}.amazonaws.com/${s3Path}`;
-
         return { pdfUrl, s3Path };
+    }
+
+    async generateAndUploadPDF(htmlContent: string, user: any): Promise<{ pdfUrl: string; s3Path: string }> {
+        const pdfBuffer = await this.generatePDF(htmlContent);
+        return this.uploadPDF(pdfBuffer, user);
     }
 }
